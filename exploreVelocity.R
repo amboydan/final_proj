25a27594ed08289ed9c605d1908c6ac50dcca50e
amboydan <amboydan@protonmail.com>
2023-04-19 15:02:46 -0800

    starting the report

--- exploreVelocity.R
+++ exploreVelocity.R
@@ -5,8 +5,8 @@ vel$rfl <- as.factor(vel$rfl)
 vel$rfl <- recode(vel$rfl, '1' = 'Rifle 1', '2' = 'Rifle 2')
 
 p <- ggplot(vel, aes(amo, vel)) + 
-  geom_jitter(width = .2, shape = 21, 
-              aes(fill = rfl), size = 3) +
+  geom_jitter(width = .15, shape = 21, 
+              aes(fill = rfl), size = 5) +
   geom_boxplot(fill = 'white', color = 'black', alpha = 0.5) +
   scale_y_continuous(label = comma) +
   ggtitle('Ammunition vs Velocity Boxplot \n') +
@@ -15,18 +15,23 @@ p <- ggplot(vel, aes(amo, vel)) +
   theme_bw() +
   theme(
     plot.title = element_text(color = 'black', face = 'bold', 
-                              hjust = 0.5, size = 12),
+                              hjust = 0.5, size = 14),
     axis.title = element_text(color = 'black', face = 'bold', 
-                              hjust = 0.5, size = 10),
+                              hjust = 0.5, size = 14),
     axis.text = element_text(color = 'black', face = 'bold', 
-                              hjust = 0.5, size = 10),
-    plot.margin = margin(c(.2,.2,.2,.2), unit = 'in')
+                              hjust = 0.5, size = 14),
+    plot.margin = margin(c(.2,.2,.2,.2), unit = 'in'),
+    legend.title = element_blank(),
+    legend.text = element_text(color = 'black', face = 'bold', 
+                               hjust = 0.5, size = 12),
+    legend.background = element_rect(color = 'black'),
+    legend.position = c(0.9, 0.1)
   )
 print(p)
 
 p2 <- ggplot(vel, aes(rfl, vel)) + 
-  geom_jitter(width = .2, shape = 21, 
-              aes(fill = amo), size = 3) +
+  geom_jitter(width = .15, shape = 21, 
+              aes(fill = amo), size = 5) +
   geom_boxplot(fill = 'white', color = 'black', alpha = 0.5) +
   scale_y_continuous(label = comma) +
   ggtitle('Rifle vs Velocity Boxplot \n') +
@@ -35,13 +40,17 @@ p2 <- ggplot(vel, aes(rfl, vel)) +
   theme_bw() +
   theme(
     plot.title = element_text(color = 'black', face = 'bold', 
-                              hjust = 0.5, size = 12),
+                              hjust = 0.5, size = 14),
     axis.title = element_text(color = 'black', face = 'bold', 
-                              hjust = 0.5, size = 10),
+                              hjust = 0.5, size = 14),
     axis.text = element_text(color = 'black', face = 'bold', 
-                             hjust = 0.5, size = 10),
+                             hjust = 0.5, size = 14),
     plot.margin = margin(c(.2,.2,.2,.2), unit = 'in'),
-    legend.title = element_blank()
+    legend.title = element_blank(),
+    legend.text = element_text(color = 'black', face = 'bold', 
+                               hjust = 0.5, size = 12),
+    legend.background = element_rect(color = 'black'),
+    legend.position = c(0.9, 0.85)
   )
 print(p2)
 
@@ -49,18 +58,23 @@ p3 <- ggplot(vel, aes(cnt, vel)) +
   geom_point(shape = 21, 
               aes(fill = amo), size = 3) +
   scale_y_continuous(label = comma) +
-  ggtitle('Shot Count vs Velocity \n') +
-  xlab('\n Shot Count') +
+  ggtitle('Shot Number vs Velocity \n') +
+  xlab('\n Shot Number') +
   ylab('Velocity (fps) \n') +
   theme_bw() +
   theme(
     plot.title = element_text(color = 'black', face = 'bold', 
-                              hjust = 0.5, size = 12),
+                              hjust = 0.5, size = 14),
     axis.title = element_text(color = 'black', face = 'bold', 
-                              hjust = 0.5, size = 10),
+                              hjust = 0.5, size = 14),
     axis.text = element_text(color = 'black', face = 'bold', 
-                             hjust = 0.5, size = 10),
-    plot.margin = margin(c(.2,.2,.2,.2), unit = 'in')
+                             hjust = 0.5, size = 14),
+    plot.margin = margin(c(.2,.2,.2,.2), unit = 'in'),
+    legend.title = element_blank(),
+    legend.text = element_text(color = 'black', face = 'bold', 
+                               hjust = 0.5, size = 12),
+    legend.background = element_rect(color = 'black'),
+    legend.position = c(0.95, 0.6)
   )
 print(p3)
 
@@ -68,6 +82,6 @@ mod <- aov(vel ~ rfl+amo , data = vel)
 # Tukey test HSD
 tukey.test <- TukeyHSD(mod)
 plot(tukey.test)
-marginal <- lsmeans::lsmeans(mod, pairwise ~ rfl+amo, adjust = 'tukey')
+marginal <- lsmeans::lsmeans(mod, pairwise ~ amo, adjust = 'tukey')
 
 
